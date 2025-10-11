dd7d99e05f34087f16ba7ab3d116c01ff879142d
Dre <92935512+JPAVictoria@users.noreply.github.com>
2025-10-11 15:22:24 +0800

    feat: singing

---
+++ app/constants/singing/constants.js
@@ -0,0 +1,8 @@
+export const SINGING_LOGIN_DATA = {
+  images: ['/singing/singing-1.png', '/singing/singing-2.png'],
+
+  content: {
+    mainTitle: 'Lyceum of Alabang',
+    subtitle: 'Trio Singing'
+  }
+}
