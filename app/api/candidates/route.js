f59a20916708eac43523f1eb85616e58c146ad46
Dre <92935512+JPAVictoria@users.noreply.github.com>
2025-10-19 19:15:41 +0800

    FEAT: ADD LEVEL CHECK

--- app/api/candidates/route.js
+++ app/api/candidates/route.js
@@ -23,6 +23,7 @@ export async function POST(request) {
       where: {
         candidateNumber: parseInt(candidateNumber),
         competition,
+        level,
         deleted: false
       }
     })
